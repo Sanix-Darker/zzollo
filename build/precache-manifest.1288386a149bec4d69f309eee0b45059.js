613c2687ddc85807b700273f3b232e02c47ee38b
sanix-darker <s4nixd@gmail.com>
2020-06-16 18:10:40 +0100

    Adding build for the github-page

---
+++ build/precache-manifest.1288386a149bec4d69f309eee0b45059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa0287d66bf034b541d0a6c282493d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8890062db7e38c9779d",
+    "url": "/static/css/main.325cf657.chunk.css"
+  },
+  {
+    "revision": "548082b7cd4e38c44a3a",
+    "url": "/static/js/2.6310e993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6310e993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8890062db7e38c9779d",
+    "url": "/static/js/main.71b78009.chunk.js"
+  },
+  {
+    "revision": "a01ab7df95c6fb2db662",
+    "url": "/static/js/runtime-main.5dfc363f.js"
+  }
+]);
