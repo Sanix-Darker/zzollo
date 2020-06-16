97f909ea7c5126555b4adba423f429c953b241e5
sanix-darker <s4nixd@gmail.com>
2020-06-16 22:22:27 +0100

    Update ItemList result number

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -66,7 +66,7 @@ class ItemList extends Component {
         search = search.toLowerCase();
 
         let source = "github";
-        fetch(this.state.links[source]["link"] + search + "&page=1&per_page=100")
+        fetch(this.state.links[source]["link"] + search + "&page=1&per_page=300")
         .then(async response => {
             let resData = await response.json();
             
