d666cf4390a88b867d2e9bee745ff8a815707322
darker <s4nixd@gmail.com>
2022-08-13 22:38:27 +0200

    feat: refactor the search box (#51)

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -14,18 +14,12 @@ class SearchBox extends Component {
         go_search: false
       }
     }
-    
 
     refreshParentState(){
         // We send those parameters to the parent
         this.props.handleState(this.state);
     }
 
-
-    /**
-     * 
-     * @param {*} event 
-     */
     handle_change(event){
         if (event.key === 'Enter'){
             this.setState({go_search: true});
@@ -37,75 +31,30 @@ class SearchBox extends Component {
         this.refreshParentState();
     }
 
-
-    /**
-     * 
-     * @param {*} event 
-     * @param {*} type 
-     */
     handle_change_option(event, type){
-        switch(type) {
-            case "source":
-                this.setState({
-                    source: event.target.selectedOptions[0].value,
-                    go_search: true
-                });
-                break;
-            case "lang":
-                this.setState({
-                    language: event.target.selectedOptions[0].value,
-                    go_search: true
-                });
-                break;
-            case "sort":
-                this.setState({
-                    sort: event.target.selectedOptions[0].value,
-                    go_search: true
-                });
-                break;
-            case "order":
-                this.setState({
-                    order: event.target.selectedOptions[0].value,
-                    go_search: true
-                })
-                break;
-            default:
-                console.log("Nothing to do....")
+        if (["source", "lang", "sort", "order"].indexOf(type) !== -1){
+            let toUpdate = {"go_search": true}
+            toUpdate[type] = event.target.selectedOptions[0].value
+            this.setState(toUpdate);
         }
         this.refreshParentState();
     }
 
-    componentWillReceiveProps = (nextProps) => {
-    }
-
-    componentDidMount(){
-    }
 
     render () {
         return (
             <div>
                 <div className="row">
                     <div className="col-md-12">
-                        <input type="text"
-                            className="search-zone"
-                            onKeyDown = {(event) => {
-                                this.handle_change(event);
-                                this.handle_change(event);
-                                this.handle_change(event);
-                            }}
-                            placeholder="Search keyword(s) for open-source project(s)..."
-                            />
+                        <input type="text" className="search-zone"
+                            onKeyUp = {(event) => this.handle_change(event)}
+                            placeholder="Search keyword(s) for open-source project(s)..." />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-3 zone">
-                        <select className="language-zone"
-                                defaultValue="all"
-                                onChange = {(event) => {
-                                    this.handle_change_option(event, "source");
-                                    this.handle_change_option(event, "source");
-                                    this.handle_change_option(event, "source");
-                                }}>
+                        <select className="language-zone" defaultValue="all"
+                                onChange={(event) => this.handle_change_option(event, "source")}>
                         <option value="all">From [Github / GitLab / Bitbucket]</option>
                         <option value="github">GitHub</option>
                         <option value="gitlab">GitLab</option>
@@ -113,42 +62,28 @@ class SearchBox extends Component {
                         </select>
                     </div>
                     <div className="col-md-3 zone">
-                        <select className="language-zone"
-                                defaultValue="all"
-                                onChange = {(event) => {
-                                    this.handle_change_option(event, "lang");
-                                    this.handle_change_option(event, "lang");
-                                    this.handle_change_option(event, "lang");
-                                }}>
-                        <option value="all">By languages</option>
-                        {this.props.LanguagesOption}
+                        <select className="language-zone" defaultValue="all"
+                                onChange = {(event) => this.handle_change_option(event, "lang")}>
+                            <option value="all">By languages</option>
+                            {this.props.LanguagesOption}
                         </select>
                     </div>
                     <div className="col-md-3 zone">
                         <select className="sort-zone"
                                 defaultValue="all"
-                                onChange = {(event) => {
-                                    this.handle_change_option(event, "sort");
-                                    this.handle_change_option(event, "sort");
-                                    this.handle_change_option(event, "sort");
-                                }}>
-                        <option value="all">By (Stars / Issues / fork)</option>
-                        <option value="star">Sort by Stars</option>
-                        <option value="issue">Sort by Issues</option>
-                        <option value="fork">Sort by Forks</option>
+                                onChange = {(event) => this.handle_change_option(event, "sort")}>
+                            <option value="all">By (Stars / Issues / fork)</option>
+                            <option value="star">Sort by Stars</option>
+                            <option value="issue">Sort by Issues</option>
+                            <option value="fork">Sort by Forks</option>
                         </select>
                     </div>
                     <div className="col-md-3 zone">
-                        <select className="order-zone"
-                                defaultValue="all"
-                                onChange = {(event) => {
-                                    this.handle_change_option(event, "order");
-                                    this.handle_change_option(event, "order");
-                                    this.handle_change_option(event, "order");
-                                }}>
-                        <option value="all">Filter by (Acsending/Descending))</option>
-                        <option value="asc">Ascending order</option>
-                        <option value="desc">Descending order</option>
+                        <select className="order-zone" defaultValue="all"
+                                onChange = {(event) => this.handle_change_option(event, "order")}>
+                            <option value="all">Filter by (Acsending/Descending))</option>
+                            <option value="asc">Ascending order</option>
+                            <option value="desc">Descending order</option>
                         </select>
                     </div>
                 </div>
