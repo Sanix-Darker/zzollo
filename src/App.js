c3873f34e2777da02ebf819312494d4316ade572
unknown <dangvu0502@gmail.com>
2022-08-16 17:02:27 +0700

    Add ability to collapse filters on click search box

--- src/App.js
+++ src/App.js
@@ -133,68 +133,78 @@ class App extends Component {
                 />
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-3 zone">
-                <input
-                  list="source"
-                  placeholder="From [Github / GitLab / Bitbucket]"
-                  className="source-zone"
-                  onKeyUp={(event) =>
-                    this.handle_change_option(event, "source")
-                  }
-                />
-                <datalist id="source">
-                  <option value="all">
-                    From [Github / GitLab / Bitbucket]
-                  </option>
-                  <option value="github">https://gitHub.com </option>
-                  <option value="gitlab">https://gitlab.com </option>
-                  <option value="bitbucket">https://bitbucket.org </option>
-                </datalist>
+            {this.state.collapseFilters && (
+              <div className="row">
+                <div className="col-md-3 zone">
+                  <input
+                    list="source"
+                    placeholder="From [Github / GitLab / Bitbucket]"
+                    className="source-zone"
+                    onKeyUp={(event) =>
+                      this.handle_change_option(event, "source")
+                    }
+                  />
+                  <datalist id="source">
+                    <option value="all">
+                      From [Github / GitLab / Bitbucket]
+                    </option>
+                    <option value="github">https://gitHub.com </option>
+                    <option value="gitlab">https://gitlab.com </option>
+                    <option value="bitbucket">https://bitbucket.org </option>
+                  </datalist>
+                </div>
+                <div className="col-md-3 zone">
+                  <input
+                    list="languages"
+                    className="language-zone"
+                    placeholder="Filter by languages"
+                    onKeyUp={(event) =>
+                      this.handle_change_option(event, "language")
+                    }
+                  />
+                  <datalist id="languages">
+                    <option value="all">By languages</option>
+                    {LanguagesOption}
+                  </datalist>
+                </div>
+                <div className="col-md-3 zone">
+                  <input
+                    list="sort"
+                    className="sort-zone"
+                    placeholder="Filter By (Stars / Issues / fork)"
+                    onKeyUp={(event) =>
+                      this.handle_change_option(event, "sort")
+                    }
+                  />
+                  <datalist id="sort">
+                    <option value="all">
+                      Filter By (Stars / Issues / fork)
+                    </option>
+                    <option value="star">Sort by Stars</option>
+                    <option value="issue">Sort by Issues</option>
+                    <option value="fork">Sort by Forks</option>
+                  </datalist>
+                </div>
+                <div className="col-md-3 zone">
+                  <input
+                    list="order"
+                    className="order-zone"
+                    placeholder="Filter by (Acsending/Descending))"
+                    defaultValue=""
+                    onKeyUp={(event) =>
+                      this.handle_change_option(event, "order")
+                    }
+                  />
+                  <datalist id="order">
+                    <option value="all">
+                      Filter by (Acsending/Descending))
+                    </option>
+                    <option value="asc">Ascending order</option>
+                    <option value="desc">Descending order</option>
+                  </datalist>
+                </div>
               </div>
-              <div className="col-md-3 zone">
-                <input
-                  list="languages"
-                  className="language-zone"
-                  placeholder="Filter by languages"
-                  onKeyUp={(event) =>
-                    this.handle_change_option(event, "language")
-                  }
-                />
-                <datalist id="languages">
-                  <option value="all">By languages</option>
-                  {LanguagesOption}
-                </datalist>
-              </div>
-              <div className="col-md-3 zone">
-                <input
-                  list="sort"
-                  className="sort-zone"
-                  placeholder="Filter By (Stars / Issues / fork)"
-                  onKeyUp={(event) => this.handle_change_option(event, "sort")}
-                />
-                <datalist id="sort">
-                  <option value="all">Filter By (Stars / Issues / fork)</option>
-                  <option value="star">Sort by Stars</option>
-                  <option value="issue">Sort by Issues</option>
-                  <option value="fork">Sort by Forks</option>
-                </datalist>
-              </div>
-              <div className="col-md-3 zone">
-                <input
-                  list="order"
-                  className="order-zone"
-                  placeholder="Filter by (Acsending/Descending))"
-                  defaultValue=""
-                  onKeyUp={(event) => this.handle_change_option(event, "order")}
-                />
-                <datalist id="order">
-                  <option value="all">Filter by (Acsending/Descending))</option>
-                  <option value="asc">Ascending order</option>
-                  <option value="desc">Descending order</option>
-                </datalist>
-              </div>
-            </div>
+            )}
           </div>
           <ItemList
             search={this.state.search}
